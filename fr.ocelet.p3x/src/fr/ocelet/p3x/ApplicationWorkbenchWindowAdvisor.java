2787bbd92299ff7f4040beafb8e9ae790f6beae6
padeg <pdegenne@teledetection.fr>
2015-02-24 11:30:22 +0400

    Created p3x platform with a basic perspective.

---
+++ fr.ocelet.p3x/src/fr/ocelet/p3x/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,28 @@
+package fr.ocelet.p3x;
+
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
+
+	public ActionBarAdvisor createActionBarAdvisor(
+			IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	public void preWindowOpen() {
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(1024, 768));
+		configurer.setShowCoolBar(false);
+		configurer.setShowStatusLine(false);
+		configurer.setShowProgressIndicator(true);
+		configurer.setTitle("Ocelet Modelling Platform");
+	}
+}
