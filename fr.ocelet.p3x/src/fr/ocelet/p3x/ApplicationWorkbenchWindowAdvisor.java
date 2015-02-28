9771c40aab258e58638498a944c71f83f3f3832f
padeg <pdegenne@teledetection.fr>
2015-02-28 10:44:47 +0400

    Removed unwanted menus and toolbar icons.

--- fr.ocelet.p3x/src/fr/ocelet/p3x/ApplicationWorkbenchWindowAdvisor.java
+++ fr.ocelet.p3x/src/fr/ocelet/p3x/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,8 @@
 package fr.ocelet.p3x;
 
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
@@ -8,7 +10,8 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
-	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+	public ApplicationWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
 		super(configurer);
 	}
 
@@ -24,5 +27,19 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		configurer.setShowStatusLine(false);
 		configurer.setShowProgressIndicator(true);
 		configurer.setTitle("Ocelet Modelling Platform");
+	}
+
+	// Remove unwanted menus and toolbar default icons
+	public void postWindowOpen() {
+
+		IWorkbenchPage iwp = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage();
+
+		iwp.hideActionSet("org.eclipse.search.searchActionSet");
+		iwp.hideActionSet("org.eclipse.ui.externaltools.ExternalToolsSet");
+		iwp.hideActionSet("org.eclipse.ui.edit.text.actionSet.annotationNavigation");
+		iwp.hideActionSet("org.eclipse.ui.edit.text.actionSet.navigation");
+//		iwp.hideActionSet("org.eclipse.jdt.ui.text.java.actionSet.presentation");
+		iwp.hideActionSet("org.eclipse.jdt.ui.edit.text.java.toggleMarkOccurrences");
 	}
 }
