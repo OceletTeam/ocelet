da3bc826213717d928987e71d3c87ecbfab41850
padeg <pdegenne@teledetection.fr>
2015-07-30 11:19:44 +0400

    Jar export : user can now choose the distination file. Issue #14

--- fr.ocelet.platform/src/fr/ocelet/platform/handlers/ExJarGen.java
+++ fr.ocelet.platform/src/fr/ocelet/platform/handlers/ExJarGen.java
@@ -18,9 +18,15 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
@@ -42,90 +48,132 @@ public class ExJarGen extends ModelCmdHandler {
 							"Please select an element of the project you want to export.");
 			return null;
 		}
+		String modelName = selectedProject.getName();
 
-		// Generating the ant file from a template
-		try {
+		// Choose a file name and destination
+		FileDialog dialog = new FileDialog(PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getShell(), SWT.SAVE);
+		dialog.setFilterNames(new String[] { "jar files", "All Files (*.*)" });
+		dialog.setFilterExtensions(new String[] { "*.jar", "*.*" });
+		dialog.setFileName(modelName.toLowerCase() + ".jar");
+		String choosenfile = dialog.open();
+		// In case of windows file separator, we replace them to ant
+		// compatible file separator '/'
+		choosenfile = choosenfile.replace("\\", "/");
 
-			IFile classpathFile = selectedProject.getFile(".classpath");
-			InputStream iscp = classpathFile.getContents();
-			BufferedReader brcp = new BufferedReader(
-					new InputStreamReader(iscp));
-			String cpline = "";
-			StringBuffer cpathlist = new StringBuffer();
-			StringBuffer filesets = new StringBuffer();
-			while ((cpline = brcp.readLine()) != null) {
-				int deb = cpline.lastIndexOf("plugins/");
-				int fin = cpline.lastIndexOf(".jar");
-				if ((deb > 0) && (fin > 0)) {
-					String cpjar = cpline.substring(deb + 8, fin + 4);
-					cpathlist.append(cpjar + " ");
-					filesets.append("      <zipfileset src=\"${plugins.dir}/"
-							+ cpjar + "\" excludes=\"META-INF/*.SF\"/>\n");
-				}
-			}
-
-			Bundle oceletPlatformBundle = Platform
-					.getBundle("fr.ocelet.platform");
-			Path antTemplate = new Path("utils/templates/ant_exjargen.xml");
-			InputStream tinputStream = null;
-			tinputStream = FileLocator.openStream(oceletPlatformBundle,
-					antTemplate, false);
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					tinputStream));
-			IFile antIf = selectedProject.getFile("output/jargen.xml");
-			File antFile = antIf.getRawLocation().makeAbsolute().toFile();
-			antFile.createNewFile();
-			FileWriter antfw = new FileWriter(antFile);
-			String rline = null;
-			String wline = null;
-			String modelName = selectedProject.getName();
-
-			while ((rline = br.readLine()) != null) {
-				wline = rline.replaceAll("_MODELNAME_", modelName);
-				wline = wline.replaceAll("_MAINCLASS_", "fr.ocelet.model."
-						+ modelName.toLowerCase() + "." + modelName);
-				wline = wline.replaceAll("_DESTFILE_", modelName.toLowerCase()
-						+ ".jar");
-				wline = wline.replaceAll("_CPATH_", cpathlist.toString());
-				if (rline.contains("_JARFILES_"))
-					antfw.write(filesets.toString());
-				else
-					antfw.write(wline + System.getProperty("line.separator"));
-			}
-			antfw.close();
-
-			Project p = new Project(); // ant project
-			// p.setInputHandler(new PluginInputHandler());
-
-			// Defines ant console logger
-			DefaultLogger consoleLogger = new DefaultLogger();
-			MessageConsole capiBuildConsole = new MessageConsole("Generating "
-					+ modelName.toLowerCase() + ".jar", null);
-			ConsolePlugin.getDefault().getConsoleManager()
-					.addConsoles(new IConsole[] { capiBuildConsole });
-			PrintStream printStream = new PrintStream(
-					capiBuildConsole.newOutputStream());
-			consoleLogger.setErrorPrintStream(printStream);
-			consoleLogger.setOutputPrintStream(printStream);
-			consoleLogger.setMessageOutputLevel(Project.MSG_INFO);
-			p.addBuildListener(consoleLogger);
-
-			// Executes ant target
-			p.setUserProperty("ant.file", antFile.getAbsolutePath());
-			p.init();
-			ProjectHelper helper = ProjectHelper.getProjectHelper();
-			p.addReference("ant.projectHelper", helper);
-			helper.parse(p, antFile);
-			p.executeTarget(p.getDefaultTarget());
-			printStream.println("Done.");
-		} catch (BuildException be) {
-			be.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		} catch (CoreException cex) {
-			cex.printStackTrace();
-		}
+		Extjarjob job = new Extjarjob(selectedProject, choosenfile);
+		job.schedule();
 		return null;
+	}
+
+	protected class Extjarjob extends Job {
+
+		private IProject selectedProject;
+		private String modelName;
+		private String choosenfile;
+
+		public Extjarjob(IProject selectedProject, String choosenfile) {
+			super("Exporting to " + choosenfile + " ...");
+			this.selectedProject = selectedProject;
+			this.choosenfile = choosenfile;
+			modelName = selectedProject.getName();
+		}
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			monitor.beginTask("Exporting jar for " + modelName + " ...", 100);
+
+			monitor.worked(25);
+
+			// Generating the ant file from a template
+			try {
+
+				IFile classpathFile = selectedProject.getFile(".classpath");
+				InputStream iscp = classpathFile.getContents();
+				BufferedReader brcp = new BufferedReader(new InputStreamReader(
+						iscp));
+				String cpline = "";
+				StringBuffer cpathlist = new StringBuffer();
+				StringBuffer filesets = new StringBuffer();
+				while ((cpline = brcp.readLine()) != null) {
+					int deb = cpline.lastIndexOf("plugins/");
+					int fin = cpline.lastIndexOf(".jar");
+					if ((deb > 0) && (fin > 0)) {
+						String cpjar = cpline.substring(deb + 8, fin + 4);
+						cpathlist.append(cpjar + " ");
+						filesets.append("      <zipfileset src=\"${plugins.dir}/"
+								+ cpjar + "\" excludes=\"META-INF/*.SF\"/>\n");
+					}
+				}
+
+				Bundle oceletPlatformBundle = Platform
+						.getBundle("fr.ocelet.platform");
+				Path antTemplate = new Path("utils/templates/ant_exjargen.xml");
+				InputStream tinputStream = null;
+				tinputStream = FileLocator.openStream(oceletPlatformBundle,
+						antTemplate, false);
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						tinputStream));
+				IFile antIf = selectedProject.getFile("output/jargen.xml");
+				File antFile = antIf.getRawLocation().makeAbsolute().toFile();
+				antFile.createNewFile();
+				FileWriter antfw = new FileWriter(antFile);
+				String rline = null;
+				String wline = null;
+
+				while ((rline = br.readLine()) != null) {
+					wline = rline.replaceAll("_MODELNAME_", modelName);
+					wline = wline.replaceAll("_MAINCLASS_", "fr.ocelet.model."
+							+ modelName.toLowerCase() + "." + modelName);
+					wline = wline.replaceAll("_DESTFILE_", choosenfile);
+					wline = wline.replaceAll("_CPATH_", cpathlist.toString());
+					if (rline.contains("_JARFILES_"))
+						antfw.write(filesets.toString());
+					else
+						antfw.write(wline
+								+ System.getProperty("line.separator"));
+				}
+				antfw.close();
+
+				monitor.worked(25);
+
+				Project p = new Project(); // ant project
+
+				// Defines ant console logger
+				DefaultLogger consoleLogger = new DefaultLogger();
+				MessageConsole capiBuildConsole = new MessageConsole(
+						"Generating " + modelName.toLowerCase() + ".jar", null);
+				ConsolePlugin.getDefault().getConsoleManager()
+						.addConsoles(new IConsole[] { capiBuildConsole });
+				PrintStream printStream = new PrintStream(
+						capiBuildConsole.newOutputStream());
+				consoleLogger.setErrorPrintStream(printStream);
+				consoleLogger.setOutputPrintStream(printStream);
+				consoleLogger.setMessageOutputLevel(Project.MSG_INFO);
+				p.addBuildListener(consoleLogger);
+
+				// Executes ant target
+				p.setUserProperty("ant.file", antFile.getAbsolutePath());
+				p.init();
+				ProjectHelper helper = ProjectHelper.getProjectHelper();
+				p.addReference("ant.projectHelper", helper);
+				helper.parse(p, antFile);
+
+				monitor.worked(25);
+
+				p.executeTarget(p.getDefaultTarget());
+				printStream.println("Done.");
+			} catch (BuildException be) {
+				be.printStackTrace();
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			} catch (CoreException cex) {
+				cex.printStackTrace();
+			}
+
+			monitor.done();
+			return Status.OK_STATUS;
+		}
 	}
 
 }
