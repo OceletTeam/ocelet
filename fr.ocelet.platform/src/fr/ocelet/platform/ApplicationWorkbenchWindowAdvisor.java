752e878c424e6ec029924fa4e2f54561c81cc85a
padeg <pdegenne@teledetection.fr>
2015-07-30 07:44:11 +0400

    Removed unwanted spaces in the toolbar (was visible on windows versions).

--- fr.ocelet.platform/src/fr/ocelet/platform/ApplicationWorkbenchWindowAdvisor.java
+++ fr.ocelet.platform/src/fr/ocelet/platform/ApplicationWorkbenchWindowAdvisor.java
@@ -1,5 +1,6 @@
 package fr.ocelet.platform;
 
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
@@ -29,7 +30,9 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		configurer.setTitle("Ocelet Modelling Platform");
 	}
 
-	// Remove unwanted menus and toolbar default icons
+	// Remove unwanted menus and toolbar default icons,
+	// and reduce de extra white space on the left of
+	// the toolbar (visible only on Windows version)
 	public void postWindowOpen() {
 
 		IWorkbenchPage iwp = PlatformUI.getWorkbench()
@@ -40,5 +43,30 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		iwp.hideActionSet("org.eclipse.ui.edit.text.actionSet.annotationNavigation");
 		iwp.hideActionSet("org.eclipse.ui.edit.text.actionSet.navigation");
 		iwp.hideActionSet("org.eclipse.jdt.ui.edit.text.java.toggleMarkOccurrences");
+		iwp.hideActionSet("org.eclipse.debug.ui.launchActionSet");
+
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		IContributionItem[] icit = configurer.getActionBarConfigurer()
+				.getCoolBarManager().getItems();
+		for (IContributionItem ici : icit) {
+			if (ici.getId().compareTo("org.eclipse.debug.ui.launch.toolbar") == 0) {
+				ici.setVisible(false);
+			}
+			if (ici.getId().compareTo("org.eclipse.debug.ui.main.toolbar") == 0) {
+				ici.setVisible(false);
+			}
+			if ((ici.getId().compareTo("additions") == 0)
+					|| (ici.getId().compareTo(
+							"org.eclipse.debug.ui.launchActionSet") == 0)
+					|| (ici.getId().compareTo(
+							"org.eclipse.search.searchActionSet") == 0)
+					|| (ici.getId()
+							.compareTo(
+									"org.eclipse.ui.edit.text.actionSet.annotationNavigation") == 0)
+					|| (ici.getId().compareTo(
+							"org.eclipse.ui.edit.text.actionSet.navigation") == 0))
+				configurer.getActionBarConfigurer().getCoolBarManager()
+						.remove(ici);
+		}
 	}
 }
