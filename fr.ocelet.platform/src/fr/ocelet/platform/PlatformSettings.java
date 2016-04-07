a61a5d9f535147cb005c59b151d45cca051349e7
padeg <pdegenne@teledetection.fr>
2016-04-07 15:36:41 +0400

    Added missing empty dirs to version control. Updated little cosmetic details.

--- fr.ocelet.platform/src/fr/ocelet/platform/PlatformSettings.java
+++ fr.ocelet.platform/src/fr/ocelet/platform/PlatformSettings.java
@@ -40,7 +40,7 @@ import org.eclipse.core.runtime.Platform;
  */
 public class PlatformSettings {
 
-	public final static String version = "1.2.0 - Beta";
+	public final static String version = "Sulfur : 2.0.1.Alpha";
 
 	public final static int NORMAL = 0;
 	public final static int VERBOSE = 1;
